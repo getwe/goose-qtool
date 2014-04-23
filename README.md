dd0765521aaa565907dfcb916112993bd203eeb4
honggengwei <honggengwei@gmail.com>
2014-04-23 23:05:38 +0800

    add files

--- README.md
+++ README.md
@@ -1,4 +1,11 @@
-goose-qtool
-===========
+#goose-qtool
 
-查询goose的命令行工具
+查询goose的命令行工具.
+
+把输入命令行转化为二进制buffer发给server,等待server返回结果后直接打印退出.使用方法:
+    
+    goose-qtool -i 127.0.0.1 -p 8808 -c '{"query":"test"}'
+    
+主要在于配合goose的demo使用,比如
+
+[goose-demo](https://github.com/getwe/goose-demo)
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+    "fmt"
+    "os"
+    "net"
+    "bytes"
+    "time"
+    "io/ioutil"
+    flags "github.com/jessevdk/go-flags"
+)
+
+func main() {
+    defer func() {
+        if r := recover();r != nil {
+            fmt.Println(r)
+            os.Exit(1)
+        }
+    }()
+
+    var opts struct {
+
+        Ip string `short:"i" long:"ip" description:"server ip"`
+
+        Port string `short:"p" long:"port" description:"server port"`
+
+        Cmd string `short:"c" long:"cmd" description:"query command"`
+
+    }
+    parser := flags.NewParser(&opts,flags.HelpFlag)
+    _,err := parser.ParseArgs(os.Args)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    if 0 == len(opts.Ip) || 0 == len(opts.Port) || 0 == len(opts.Cmd) {
+        parser.WriteHelp(os.Stderr)
+        os.Exit(1)
+    }
+    work(opts.Ip,opts.Port,opts.Cmd)
+}
+
+func work(ip,port,cmd string) {
+
+    conn,err := net.DialTimeout("tcp",fmt.Sprintf("%s:%s",ip,port),time.Second)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    reqbuf := bytes.NewBufferString(cmd)
+    _,err = conn.Write(reqbuf.Bytes())
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+
+    resbytes,err := ioutil.ReadAll(conn)
+    if err != nil {
+        fmt.Println(err)
+        os.Exit(1)
+    }
+    resbuf := bytes.NewBuffer(resbytes)
+    fmt.Println(resbuf.String())
+}
